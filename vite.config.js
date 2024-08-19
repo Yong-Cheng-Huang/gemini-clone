e5aada80ba1a5433f1bdce115b805beaf0c497f0
Yong-Cheng <frank0929649689@gmail.com>
2024-08-19 22:52:37 +0800

    update base

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// vite.config.js
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  base: '/gemini-clone/', // 這裡的路徑應該是你的 GitHub Pages 儲存庫名稱
+});
